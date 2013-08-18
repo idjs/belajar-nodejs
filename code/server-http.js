cedcc76110aaee34067263a84b876510856d1ebf
Equan Pr <eq@junwatu.com>
2013-08-18 14:22:59 +0700

    server node.js sederhana

---
+++ code/server-http.js
@@ -0,0 +1,16 @@
+var http = require('http'),
+	PORT = 3400;
+
+http.createServer(function(req, res){
+	var body = "<pre>Haruskah belajar Node.js?</pre><p><h3>...Yo Kudu! ;)</h3></p>"
+	res.writeHead(200, {
+		'Content-Length':Buffer.byteLength(body),
+		'Content-Type':'text/html',
+		'Pesan-Header':'Pengenalan Node.js'
+	});
+
+	res.write(body);
+	res.end();
+}).listen(PORT);
+
+console.log("Port "+PORT+" : Node.js Server...");
